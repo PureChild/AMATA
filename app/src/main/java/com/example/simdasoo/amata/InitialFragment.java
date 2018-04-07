656874119d569984933fc34c51a64a4cc77301d6
purechild <a01056490114@gmail.com>
2018-04-08 00:49:16 +0900

    DB 관련 test

--- app/src/main/java/com/example/simdasoo/amata/InitialFragment.java
+++ app/src/main/java/com/example/simdasoo/amata/InitialFragment.java
@@ -1,6 +1,9 @@
 package com.example.simdasoo.amata;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -15,13 +18,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.simdasoo.amata.R;
-
 
 //첫화면
 public class InitialFragment extends Fragment {
     //등록된 물품의 개수 - 추후 sql문으로 바꿔야함
-    public int cntStuff = 0;
+    int cntStuff;
+
+    DBHelper dbHelper;
+    SQLiteDatabase database;
 
     public View getView(){
         View rootview = getLayoutInflater().inflate(R.layout.initial_fragment,null);
@@ -46,6 +50,13 @@ public class InitialFragment extends Fragment {
             }
         });
 
+        //DB 연결
+        dbHelper = new DBHelper(getActivity());
+        database = dbHelper.getWritableDatabase();
+
+        //등록된 물건 수 확인
+        cntStuff = count(database);
+
         //초기화면 등록된 물건이 없을 경우
         TextView tv = (TextView) rootview.findViewById(R.id.tv_main);
         if(cntStuff == 0){
@@ -68,6 +79,12 @@ public class InitialFragment extends Fragment {
         builder.setPositiveButton("OK",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
+                        //등록 테스트
+//                        TextView tv = (TextView) getView().findViewById(R.id.tv_main);
+//                        testInsert(database, (String) tv.getText());
+
+                        //삭제 테스트
+//                        testDelete(database);
                         cntStuff++;
                         refresh();
                     }
@@ -75,9 +92,36 @@ public class InitialFragment extends Fragment {
         builder.setNeutralButton("Cancel",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getActivity(),"Cancel 버튼 클릭됨",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(),String.valueOf(count(database)),Toast.LENGTH_LONG).show();
                     }
                 });
         builder.show();
+    }
+
+    //DB에 데이터 삽입
+    private void testInsert(SQLiteDatabase database, String name){
+        ContentValues values = new ContentValues();
+        values.put("ID", name);
+        values.put("NAME", name);
+        database.insert("registered_list", null, values);
+        Toast.makeText(getActivity(),"ok",Toast.LENGTH_LONG).show();
+    }
+
+    //DB 데이터 삭제
+    private void testDelete(SQLiteDatabase database){
+        database.execSQL("DELETE FROM registered_list");
+        Toast.makeText(getActivity(),"ok",Toast.LENGTH_LONG).show();
+    }
+
+
+    //DB 데이터 개수 조회
+    public int count(SQLiteDatabase database){
+        int count = 0;
+        String sql = "select * from registered_list";
+        Cursor cursor = database.rawQuery(sql, null);
+        if(cursor != null){
+            count = cursor.getCount(); //조회된 개수얻기
+        }
+        return count;
     }
 }
